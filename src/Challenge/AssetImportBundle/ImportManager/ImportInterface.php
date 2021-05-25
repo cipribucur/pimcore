3f2310304106bd5c59d96faed4636feb26d8dd2d
Bucur Ciprian <c.bucur@agentur-loop.com>
2021-05-25 15:23:53 +0200

    add annotations to the interface

--- src/Challenge/AssetImportBundle/ImportManager/ImportInterface.php
+++ src/Challenge/AssetImportBundle/ImportManager/ImportInterface.php
@@ -9,14 +9,32 @@ use Pimcore\Model\DataObject;
 
 interface ImportInterface
 {
+    /**
+     * @param DataObject $dataObject
+     * @param $key
+     * @param $parent
+     * @return array
+     */
     public function import(DataObject $dataObject, $key, $parent): array;
 
+    /**
+     * @param array $importLocalizedAttributes
+     */
     public function setImportLocalizedAttributes(array $importLocalizedAttributes): void;
 
+    /**
+     * @param array $importAttributes
+     */
     public function setImportAttributes(array $importAttributes): void;
 
+    /**
+     * @param array $importLocales
+     */
     public function setImportLocales(array $importLocales): void;
 
+    /**
+     * @param Asset|null $asset
+     */
     public function setAsset(?Asset $asset): void;
 
 }
